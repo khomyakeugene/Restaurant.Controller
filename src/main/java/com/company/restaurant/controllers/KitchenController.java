bbab85a11b6cb89853032f083bb08e064b0bd78c
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-28 03:30:56 +0300

    Restaurant Controller

--- src/main/java/com/company/restaurant/controllers/KitchenController.java
+++ src/main/java/com/company/restaurant/controllers/KitchenController.java
@@ -1,6 +1,6 @@
 package com.company.restaurant.controllers;
 
-import com.company.restaurant.model.CookedCourseView;
+import com.company.restaurant.model.CookedCourse;
 import com.company.restaurant.model.Course;
 import com.company.restaurant.model.Employee;
 
@@ -10,9 +10,9 @@ import java.util.List;
  * Created by Yevhen on 17.06.2016.
  */
 public interface KitchenController {
-    CookedCourseView addCookedCourse(Course course, Employee employee, Float weight);
+    CookedCourse addCookedCourse(Course course, Employee employee, Float weight);
 
-    void delCookedCourse(CookedCourseView cookedCourseView);
+    void delCookedCourse(CookedCourse cookedCourse);
 
-    List<CookedCourseView> findAllCookedCourses();
+    List<CookedCourse> findAllCookedCourses();
 }
